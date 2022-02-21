fc278ec92f2b4b1c277d3a8b8681ed0b015d9f0c
eloidrai <eloidrai@gmail.com>
2022-02-21 23:15:35 +0100

    Fix bugs

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 # include <SDL.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 # include <stdbool.h>
 
 # include "vectors.h"
@@ -8,7 +9,7 @@
 # define WIDTH 1500
 # define HEIGHT 800
 
-# define SCALE 0.00002 // taille d'un m en px
+# define SCALE 0.00003 // taille d'un m en px
 # define ISS_MASS 450000
 # define EARTH_RADIUS 6371000
 
@@ -18,7 +19,6 @@ void drawObject(SDL_Renderer * renderer, int x, int y) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderFillRect(renderer, &r1);
 	SDL_RenderFillRect(renderer, &r2);
-	SDL_RenderPresent(renderer);
 }
 
 
@@ -27,6 +27,8 @@ int main(int argc, char **argv) {
 	SDL_Renderer * renderer;
 	SDL_Surface * earth_surface;
 	SDL_Texture * earth_texture;
+	SDL_Texture * background_texture;
+	struct timespec duration = {0, 40000000};
 
 	if (SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr, "Init failed\n");
@@ -47,44 +49,36 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 	
-	SDL_SetRenderDrawColor(renderer, 0, 0, 50, 255);
-	SDL_RenderClear(renderer);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderDrawLine(renderer, 0, HEIGHT/2, WIDTH, HEIGHT/2);
-	SDL_RenderDrawLine(renderer, WIDTH/2, 0, WIDTH/2, HEIGHT);
-	SDL_RenderPresent(renderer);
-
+	background_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
 	earth_texture = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("dessin-de-la-terre.bmp"));
-
-	SDL_Rect dest = {WIDTH/2-EARTH_RADIUS*SCALE, HEIGHT/2-EARTH_RADIUS*SCALE, EARTH_RADIUS*SCALE*2, EARTH_RADIUS*SCALE*2};
-	SDL_RenderCopy(renderer, earth_texture, NULL, &dest);
-
-	SDL_RenderPresent(renderer);
+	SDL_Rect earth_dest = {WIDTH/2-EARTH_RADIUS*SCALE, HEIGHT/2-EARTH_RADIUS*SCALE, EARTH_RADIUS*SCALE*2, EARTH_RADIUS*SCALE*2};
+	
+	SDL_SetRenderTarget(renderer, background_texture);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 50, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderDrawLine(renderer, 0, HEIGHT/2, WIDTH, HEIGHT/2);
+		SDL_RenderDrawLine(renderer, WIDTH/2, 0, WIDTH/2, HEIGHT);
+		SDL_RenderCopy(renderer, earth_texture, NULL, &earth_dest);
+	SDL_SetRenderTarget(renderer, NULL);
 
 	struct PhysicalObject Earth = {{0, 0}, {0, 0}, 5.972e24};
-	struct PhysicalObject ISS = {{6750000, 0}, {0, 9500},  ISS_MASS};  // Actual speed ist about 7777 m/s
+	struct PhysicalObject ISS = {{6750000, 0}, {0, 7777},  ISS_MASS};  // Actual speed ist about 7777 m/s
+	struct Vector a;
 	SDL_Event event;
-	int time = 0;
 	while (true) {
 		if (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				return EXIT_SUCCESS;
 			}
 		}
-		struct Vector a = get_acceleration_vector(&Earth, &ISS);
-/*		printf("Accélération causée par la terre : x = %lf, y = %lf\n", a.x, a.y);
-		printf("Position de l'ISS : x = %lf, y = %lf\n", ISS.pos.x, ISS.pos.y);
-		printf("Mouvement de l'ISS : x = %lf, y = %lf\n\n", ISS.movement.x, ISS.movement.y);
-*/
+		a = get_acceleration_vector(&Earth, &ISS);
+		
 		ISS.movement = add(&ISS.movement, &a);
 		ISS.pos.x += ISS.movement.x;
 		ISS.pos.y += ISS.movement.y;
-//		if (ISS.pos.x < 6371000) return 0;
-		time++;
-		if (time % 150) continue;
-//		printf("%d\n", time);
+		SDL_RenderCopy(renderer, background_texture, NULL, NULL);
 		drawObject(renderer, WIDTH/2+ISS.pos.x*SCALE, HEIGHT/2+ISS.pos.y*SCALE);
 		SDL_RenderPresent(renderer);
-//		if (ISS.pos.x < 0) return 0;
 	}
 }
